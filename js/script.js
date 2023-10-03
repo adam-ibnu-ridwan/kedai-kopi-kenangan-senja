9260aa8cfdce86e676d73e4efb7d55bd08339190
Adam Ibnu Ridwan <89550112+adam-ibnu-ridwan@users.noreply.github.com>
2023-10-03 15:53:48 +0700

    Update script.js
    adding alert caption

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+alert(`
+referensi: https://www.youtube.com/watch?v=MCVkMmYL-aY&pp=ygUTa29waSBrZW5hbmdhbiBzZW5qYQ%3D%3D
+ini bukan website sungguhan, & hampir 100% sama dengan tutorialnya... sekedar bahan pembelajaran & histori pembelajaran pribadi :)
+`);
+
 const hamMenu = document.getElementById("ham-menu");
 const navbarNav = document.querySelector(".navbar-nav");
 

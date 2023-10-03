0b2cb0cfbef5a510fbc848f8f764fbcb099e05bf
Adam Ibnu Ridwan <89550112+adam-ibnu-ridwan@users.noreply.github.com>
2023-10-03 17:19:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-alert("referensi: https://www.youtube.com/watch?v=MCVkMmYL-aY&pp=ygUTa29waSBrZW5hbmdhbiBzZW5qYQ%3D%3D\nini bukan website sungguhan, & hampir 100% sama dengan tutorialnya... sekedar bahan pembelajaran & histori pembelajaran pribadi :)");
-
 const hamMenu = document.getElementById("ham-menu");
 const navbarNav = document.querySelector(".navbar-nav");
 
